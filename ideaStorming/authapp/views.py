be944f5137587e59edbe671c4a65213b4c2a70d5
gustavo <pfeifer.g@gmail.com>
2017-02-12 22:33:28 -0400

    se creo authapp

---
+++ ideaStorming/authapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+"""
+ref:
+https://docs.djangoproject.com/en/1.10/topics/auth/default/#how-to-log-a-user-in
+https://docs.djangoproject.com/en/1.10/topics/auth/default/#how-to-log-a-user-out
+"""
+
