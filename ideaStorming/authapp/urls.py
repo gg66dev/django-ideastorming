9e5135df19bda5c03019db08c69a19cad89716d7
gustavo <pfeifer.g@gmail.com>
2017-02-16 16:41:10 -0400

    added bootstrap and was setup static files

--- ideaStorming/authapp/urls.py
+++ ideaStorming/authapp/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^register/', views.UserRegister.as_view()),
-    url(r'^login/', views.login),
-    url(r'^logout/', views.logout),
-
+    url(r'^register/$', views.UserRegister.as_view()),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout),
 ]
--- ideaStorming/ideaStorming/urls.py
+++ ideaStorming/ideaStorming/urls.py
@@ -1,8 +1,10 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles import views
 
 urlpatterns = [
     url(r'^',include('authapp.urls')),
+    url(r'^',include('projectapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
