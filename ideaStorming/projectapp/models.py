7ed7e7fa2e730b8f0404d6c5ba8acb8f4cdcf0db
gustavo <pfeifer.g@gmail.com>
2017-02-18 04:41:06 -0400

    create models from project app

--- ideaStorming/projectapp/models.py
+++ ideaStorming/projectapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from authapp.models import User
+
+class Project(models.Model):
+    INVESTMENT_OPTIONS = (
+        ('< 100 USD', '< 100 USD'),
+        ('100 - 300 USD', '100 - 300 USD'),
+        ('301 - 500 USD', '301 - 500 USD'),
+        ('501 - 800 USD', '501 - 800 USD'),
+        ('801 - 1000 USD', '801 - 1000 USD'),
+        ('> 1000 USD', '> 1000 USD'),
+    )
+    
+    title = models.CharField(max_length=30) 
+    summary = models.TextField()
+    advantages = models.TextField()
+    investment = models.CharField(max_length=100, choices=INVESTMENT_OPTIONS)
+    date_creation =  models.DateField() 
+    date_last_modification =  models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
+    project = models.ManyToManyField(Project)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    score  = models.IntegerField()
+    comment = models.CharField(max_length=300)
