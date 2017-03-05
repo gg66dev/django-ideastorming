78522d0fe0d83c87e69bbc5f1a11867287720e92
gustavo <pfeifer.g@gmail.com>
2017-03-05 13:27:04 -0400

    add pagination and template style in my-projects page

--- ideaStorming/projectapp/views.py
+++ ideaStorming/projectapp/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+
+
 from authapp.views import login
 
 from .models import Project
@@ -43,9 +48,28 @@ class ProjectNewView(FormView):
 @method_decorator(login_required, name='dispatch')
 class ProjectListView(ListView):
     model = Project
-    template_name = 'list_project.html'
-     
-    def get_queryset(self):
-        qs = super(ProjectListView, self).get_queryset()
-        return qs.filter(user=self.request.user)
+    template_name = 'my_projects.html'
+    paginate_by = 15 
+
+    #def get_queryset(self):
+    #    qs = super(ProjectListView, self).get_queryset()
+    #    return qs.filter(user=self.request.user)
        
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectListView, self).get_context_data(**kwargs) 
+        qs = super(ProjectListView, self).get_queryset()
+        list_exam = qs.filter(user=self.request.user)
+        paginator = Paginator(list_exam, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            file_exams = paginator.page(page)
+        except PageNotAnInteger:
+            file_exams = paginator.page(1)
+        except EmptyPage:
+            file_exams = paginator.page(paginator.num_pages)
+
+        context['object_list'] = file_exams
+        return context
