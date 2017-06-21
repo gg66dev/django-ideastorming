3d92b52f9853c1c7076e980e45924b140311b494
gustavo <pfeifer.g@gmail.com>
2017-06-20 23:06:06 -0400

    set widget rankig according the score of the comment

--- ideaStorming/media/js/app.js
+++ ideaStorming/media/js/app.js
@@ -1,1 +1,17 @@
-﻿$('.rating').rating();
+﻿
+
+var rankingModule = (function () {
+
+    var setRanking = function (el, score) {
+        $(el).rating('rate', score);
+    }
+
+
+    return {
+        setRanking: setRanking
+    }
+
+})();
+
+
+
